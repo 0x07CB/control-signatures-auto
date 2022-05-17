cacc795634a6ec7d5509b9d66d33471200c663f0
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-05-17 22:59:26 +0200

    update main program

--- control-ssh-folder-sig.py
+++ control-ssh-folder-sig.py
@@ -6,6 +6,19 @@ from os import system
 import gzip
 import base64
 import hashlib
+from multiprocessing import Process
+
+
+def process_notify():
+    pass
+
+def process_sound_alert():
+    pass
+
+def process_espeak_alert():
+    pass
+
+
 
 class FileSigControl(object):
     def __init__(self):
@@ -27,7 +40,39 @@ class FileSigControl(object):
                 H_.update(it.encode())
                 self.files_hashed_lst.append(H_.hexdigest())
             f2.close()
+    
+    def check_circuit(self):
+        self.made_checksums_tmp_files()
+        self.made_checksums_all_files()
+        return self
+
+    def getter_dict_json_output_data(self):
+        self.construct_json_data()
+        return self.dict_json_data
+
+    def construct_json_data(self):
+        self.dict_json_data = {}
+        pass
+
+    def write_json_data(self):
+        pass
+
+    def read_json_data(self):
+        pass
+
+    def compare_version(self):
+        pass
+
+    def notify_alert(self):
+        pass
+
+    def control_processus(self):
+        return None
 
     
                 
+Fsigctrl = FileSigControl()
+# =====
+Result_of_detection = Fsigctrl.check_circuit().control_processus()
 
+exit(0)
